0ac13504cf7f9712419e2a8b1ffc883e7e36c021
Tok <lsteiger4@gmail.com>
2018-07-15 16:52:02 +0200

    Prepared synth loop.

--- src/zir_synth/listener.clj
+++ src/zir_synth/listener.clj
@@ -21,22 +21,6 @@
   (let [sample-size-in-bits 8 channels 2 signed true big-endian false]
     (AudioFormat. zir-synth/sample-rate-Hz sample-size-in-bits channels signed big-endian)))
 
-(defn play-note [velocities sdl note]
-  (loop [t 0]
-    (let [velocity @(get velocities note)
-          running? (> velocity 0)]
-      (if running?
-        (let [frequency-Hz (note/frequency note)
-              volume (* velocity 0.2)
-              data (osc/wave-bytes (osc/square t frequency-Hz) volume)
-              bytes (byte-array (concat [data] [data]))]
-          (.write ^SourceDataLine sdl bytes 0 2)
-          (recur (+ t 1)))
-        (do
-          (.drain sdl)
-          (.stop sdl)
-          (.close sdl))))))
-
 (defn note-off [velocities timestamp note]
   (println timestamp "OFF" note (note/note-name note))
   (reset! (get velocities note) 0))
@@ -45,28 +29,43 @@
   (println timestamp "ON" note velocity (note/note-name note))
   (if (= velocity 0)
     (note-off velocities timestamp note)
-    (let [audio-format (audio-format)
-          sdl (AudioSystem/getSourceDataLine audio-format)]
-      (.open sdl audio-format)
-      (.start sdl)
-      (reset! (get velocities note) velocity)
-      (go (play-note velocities sdl note))
-      )))
+    (reset! (get velocities note) velocity)))
 
 (defn zir-receiver []
-  (def velocities (into (sorted-map) (map (fn [i] [i (atom (int 0))]) (range 0 127))))
-  (reify Receiver
-    (send [_ message timestamp]
-      (let [command (int (.getCommand message))
-            channel (int (.getChannel message))]
-        (if (or (= command ShortMessage/NOTE_ON) (= command ShortMessage/NOTE_OFF))
-          (let [note (.getData1 message)
-                velocity (.getData2 message)]
-            (cond
-              (= command ShortMessage/NOTE_ON) (note-on velocities timestamp note velocity)
-              (= command ShortMessage/NOTE_OFF) (note-off velocities timestamp note)))
-          (println "WARNING: Unhandled command" command))))
-    (close [this] (println "closing..."))))
+  (let [audio-format (audio-format)
+        sdl (AudioSystem/getSourceDataLine audio-format)
+        velocities (into (sorted-map) (map (fn [i] [i (atom (int 0))]) (range 0 127)))]
+    (defn- synth-loop [t]
+      (doseq [active (filter (fn [[k v]] (> @v 0)) velocities)]
+        (let [note (key active)
+              velocity @(val active)
+              frequency-Hz (note/frequency note)
+              volume (* velocity 0.2)
+              data (osc/wave-bytes (osc/square t frequency-Hz) volume)
+              bytes (byte-array (concat [data] [data]))]
+          (.write ^SourceDataLine sdl bytes 0 2)))
+      (recur (+ t 1)))
+    (defn start-up []
+      (println "Starting receiver...")
+      (.open sdl audio-format)
+      (.start sdl)
+      (go (synth-loop 0)))
+    (reify Receiver
+      (send [_ message timestamp]
+        (let [command (int (.getCommand message))
+              channel (int (.getChannel message))]
+          (if (or (= command ShortMessage/NOTE_ON) (= command ShortMessage/NOTE_OFF))
+            (let [note (.getData1 message)
+                  velocity (.getData2 message)]
+              (cond
+                (= command ShortMessage/NOTE_ON) (note-on velocities timestamp note velocity)
+                (= command ShortMessage/NOTE_OFF) (note-off velocities timestamp note)))
+            (println "WARNING: Unhandled command" command))))
+      (close [this]
+        (println "Closing...")
+        (.drain sdl)
+        (.stop sdl)
+        (.close sdl)))))
 
 (defn midi-port? [device] (and (not (instance? Sequencer device)) (not (instance? Synthesizer device))))
 (defn in-port? [midi-port] (s/includes? (type (.getDeviceInfo midi-port)) "MidiInDevice"))
@@ -80,9 +79,11 @@
         in-port (first (filter in-port? midi-ports))
         synth-transmitter (.getTransmitter ^MidiDevice in-port)
         zir-transmitter (.getTransmitter ^MidiDevice in-port)
-        synth (MidiSystem/getSynthesizer)]
+        synth (MidiSystem/getSynthesizer)
+        rec (zir-receiver)]
     (.setReceiver synth-transmitter (.getReceiver synth))
-    (.setReceiver zir-transmitter (zir-receiver))
+    (.setReceiver zir-transmitter rec)
+    (start-up)
     (println "Trying to open device" (.toString (.getDeviceInfo in-port)))
     (try
       (.open synth)
